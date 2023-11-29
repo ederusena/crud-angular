52a0bd2d434d6c25aa8e2f86655e9a048e39be5d
Eder Sena <eder.sena@live.com>
2023-11-29 11:28:18 -0300

    por algum motivo testar tailwind no projeto

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-', // tailwind prefix
+  content: [
+    "./src/**/*.{html,scss,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

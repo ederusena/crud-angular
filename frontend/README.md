25aab4c6119afac134e8a3363ab4a9ada8ba4d69
Eder Sena <eder.sena@live.com>
2023-11-29 14:09:28 -0300

    Update frontend configuration and add shared module

--- frontend/README.md
+++ frontend/README.md
@@ -12,3 +12,25 @@ ng add @angular/material
 ng g m courses --routing
 ```
 
+## Create Shared for exports component in common
+
+```base
+ng g m shared/app-material
+```
+
+```js
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [
+    MatCardModule,
+    MatTableModule,
+    MatToolbarModule
+  ]
+})
+export class AppMaterialModule { }
+
+```
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AppMaterialModule } from './shared/app-material/app-material.module'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    AppMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
